a10a01f78c2a564ff8eaffdcc1535cbf627a8f0e
RSHKDL <julien.helfer@gmail.com>
2017-10-04 20:03:07 +0200

    Added router iteration 1

--- tests/Framework/AppTest.php
+++ tests/Framework/AppTest.php
@@ -1,11 +1,16 @@
 <?php
 namespace Tests\Framework;
 
+use App\Blog\BlogModule;
 use Framework\App;
 use GuzzleHttp\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Tests\Framework\Modules\ErroredModule;
+use Tests\Framework\Modules\StringModule;
 
-class AppTest extends TestCase {
+class AppTest extends TestCase
+{
 
     public function testRedirectTrailingSlash() {
         $app = new App();
@@ -16,16 +21,41 @@ class AppTest extends TestCase {
     }
 
     public function testBlog() {
-        $app = new App();
+        $app = new App([
+            BlogModule::class
+        ]);
         $request = new ServerRequest('GET', '/blog');
         $response = $app->run($request);
         $this->assertContains('<h1>Welcome to the Blog</h1>', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
+
+        $requestSingle = new ServerRequest('GET', '/blog/test-article');
+        $responseSingle = $app->run($requestSingle);
+        $this->assertContains('<h1>Welcome to the article : test-article</h1>', (string)$responseSingle->getBody());
+    }
+
+    public function testThrowExceptionIfNoResponseSent() {
+        $app = new App([
+            ErroredModule::class
+        ]);
+        $request = new ServerRequest('GET', '/demo');
+        $this->expectException(\Exception::class);
+        $app->run($request);
+    }
+
+    public function testConvertStringToResponse() {
+        $app = new App([
+            StringModule::class
+        ]);
+        $request = new ServerRequest('GET', '/demo');
+        $response = $app->run($request);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals('DEMO', (string)$response->getBody());
     }
 
     public function testError404() {
         $app = new App();
-        $request = new ServerRequest('GET', '/urlthatdontexist');
+        $request = new ServerRequest('GET', '/fakeurl');
         $response = $app->run($request);
         $this->assertContains('<h1>Erreur 404</h1>', (string)$response->getBody());
         $this->assertEquals(404, $response->getStatusCode());
